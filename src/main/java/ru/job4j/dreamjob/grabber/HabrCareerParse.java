8fd9dca3075d9a8ea153c2b48384f4f79d2d87ac
Andrey Laktyushin <andrey@laktyushin.com>
2022-09-10 13:33:24 +0300

    Парсинг HTML страницы (Habr.Career)

---
+++ src/main/java/ru/job4j/dreamjob/grabber/HabrCareerParse.java
@@ -0,0 +1,29 @@
+package ru.job4j.dreamjob.grabber;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class HabrCareerParse {
+
+    private static final String SOURCE_LINK = "https://career.habr.com";
+
+    private static final String PAGE_LINK = SOURCE_LINK.concat("/vacancies/java_developer");
+
+    public static void main(String[] args) throws IOException {
+        Connection connection = Jsoup.connect(PAGE_LINK);
+        Document document = connection.get();
+        Elements rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            Element titleElement = row.select(".vacancy-card__title").first();
+            Element linkElement = titleElement.child(0);
+            String vacancyName = titleElement.text();
+            String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
+            System.out.printf("%s %s%n", vacancyName, link);
+        });
+    }
+}
